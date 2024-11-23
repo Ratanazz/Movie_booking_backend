7c95c47cf160376831017c206f57e8130684348c
Ratana <rathanachan55@gmail.com>
2024-11-23 16:29:04 +0700

    new with backpack

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
+    use CrudTrait;
     public function movies()
     {
         return $this->hasMany(Movie::class);
